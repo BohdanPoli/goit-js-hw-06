9cb33cded6344381bd1f86f7d2d615396960d7c6
Bohdan Polishchuk <polishuk.bog@gmail.com>
2022-11-09 20:52:51 +0200

    finish fourth task

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,22 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const value = document.querySelector("#value");
+
+let counterValue = Number(value.textContent);
+
+const decrementRef = document.querySelector(
+  'button[data-action = "decrement"]'
+);
+const incrementRef = document.querySelector(
+  'button[data-action = "increment"]'
+);
+
+const decrement = () => (value.textContent = counterValue -= 1);
+const increment = () => (value.textContent = counterValue += 1);
+
+decrementRef.addEventListener("click", decrement);
+incrementRef.addEventListener("click", increment);
+
+// Не знаю, наскільки це правильне рішення
