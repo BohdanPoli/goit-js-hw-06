26e0b1ffb911f54976a92b7ffb9ee016a24829ea
Bohdan Polishchuk <polishuk.bog@gmail.com>
2022-11-12 17:51:30 +0200

    finish fifth task

---
+++ js/task-05.js
@@ -0,0 +1,20 @@
+// Напиши скрипт, який під час набору тексту в
+//  інпуті input#name - input(подія input), підставляє
+// його поточне значення в span#name - output.Якщо інпут
+// порожній, у спані повинен відображатися рядок "
+// Anonymous".
+
+const ref = {
+  input: document.querySelector("#name-input"),
+  span: document.querySelector("#name-output"),
+};
+
+function onInputText() {
+  if (event.currentTarget.value === "") {
+    ref.span.textContent = "Anonymous";
+  } else {
+    ref.span.textContent = event.currentTarget.value;
+  }
+}
+
+ref.input.addEventListener("input", onInputText);
